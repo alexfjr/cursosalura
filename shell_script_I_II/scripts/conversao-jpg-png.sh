10f2b294aebf89fe5967c5e27cea3c1142437da3
Alexandro Ferreira Júnior <alexandro_fjr@hotmail.com>
2018-06-28 16:46:39 -0300

    Script aprimorado para utilizar uma function, exibir uma mensagem do resultado e direcionar a saída de erro para um arquivo.

--- shell_script_I_II/scripts/conversao-jpg-png.sh
+++ shell_script_I_II/scripts/conversao-jpg-png.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-cd ~/Documents/Learning/cursosalura/shell_script_I_II/imagens-livros
+converte_imagens() {
+   cd ~/Documents/Learning/cursosalura/shell_script_I_II/imagens-livros
 
-if [ ! -d png ]
+   if [ ! -d png ]
+   then
+      mkdir png
+   fi
+
+   for imagem in *.jpg
+   do
+      local imagem_sem_extensao=$(ls $imagem | mawk -F . '{print $1}')
+      convert $imagem png/$imagem_sem_extensao.png
+   done
+}
+
+converte_imagens 2> erros_conversao.log
+
+if [ $? -eq 0 ]
 then
-   mkdir png
+   echo "Conversão realizada com sucesso!"
+else
+   echo "Ocorreu um problema na conversão!"
 fi
-
-for imagem in *.jpg
-do
-    imagem_sem_extensao=$(ls $imagem | mawk -F . '{print $1}')
-    convert $imagem png/$imagem_sem_extensao.png
-done
