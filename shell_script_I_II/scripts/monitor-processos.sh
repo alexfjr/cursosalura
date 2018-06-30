53d7064cd9edb70a06db11351ceee708de5a2e76
Alexandro Ferreira Júnior <alexandro_fjr@hotmail.com>
2018-06-30 15:51:30 -0300

    Iniciando a criação do script de monitoramento de processos

---
+++ shell_script_I_II/scripts/monitor-processos.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+processos=$(ps -e -o pid --sort -size | head -n 11 | grep [0-9])
+
+for pid in $processos
+do
+   nome_processo=$(ps -p $pid -o comm=)
+   echo -n $(date +%F,%H:%M:%S,) >> logs_processos/$nome_processo.log
+done
+
