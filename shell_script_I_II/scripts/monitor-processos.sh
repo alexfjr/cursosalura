2eaafeaf09a9d581c517ef9a4f12ac134c46e360
Alexandro Ferreira Júnior <alexandro_fjr@hotmail.com>
2018-06-30 16:21:21 -0300

    Finalizado o script para gerar logs dos processos que mais estão utilizando a memória RAM

--- shell_script_I_II/scripts/monitor-processos.sh
+++ shell_script_I_II/scripts/monitor-processos.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
-if [ ! -d logs_processos ]
+if [ ! -d logs ]
 then
    mkdir logs
 fi
 
 processos=$(ps -e -o pid --sort -size | head -n 11 | grep [0-9])
 
-for pid in $processos
-do
-   nome_processo=$(ps -p $pid -o comm=)
-   echo -n $(date +%F,%H:%M:%S,) >> logs/$nome_processo.log
-done
+gerar_logs() {
+   for pid in $processos
+   do
+      nome_processo=$(ps -p $pid -o comm=)
+      echo -n $(date +%F,%H:%M:%S,) >> logs/$nome_processo.log
+
+      tamanho_processo=$(ps -p $pid -o size | grep [0-9])
+      echo "$(bc <<< "scale=2;$tamanho_processo/1024") MB" >> logs/$nome_processo.log
+   done
+}
+
+gerar_logs
+
+if [ $? -eq 0 ]
+then
+   echo "Logs gerados com sucesso!"
+else
+   echo "Erro na geração dos logs!"
+fi
 
